92f1290c0d053b5727afad653ba75f76c04a9ca1
Aima <aimaajmal91203@gmail.com>
2026-08-28 13:27:50 +0500

    Version Update
    removed extra files left from V1

--- invoice-tracker/frontend/services/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Services Layer - API clients, persistence, and UI capture utilities
- */
-
-export { ApiService } from './ApiService'
-export { StatePersistence, uiPreferences, draftStorage, stateCache } from './StatePersistence'
-export type { PersistenceOptions } from './StatePersistence'
-export { UICapture, uiCapture } from './UICapture'
-export type { UISnapshot, ComponentRegistration } from './UICapture'
